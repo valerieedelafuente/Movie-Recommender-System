b1c47a228ceb13fc0d3b9ce2858309d67cfea403
lescer3 <schoolleslie3@gmail.com>
2025-03-05 21:14:51 -0800

    added more graphs

--- scripts/eda/EDA.py
+++ scripts/eda/EDA.py
@@ -17,3 +17,49 @@ plt.figure(figsize = (18,8))
 sns.heatmap(movies_df.isnull(), cmap = sns.color_palette("viridis", as_cmap = True))
 plt.show()
 
+#separating genres and exploding
+movies_df['genre_list'] = movies_df['genre_ids'].str.split(',')
+movies_exploded = movies_df.explode('genre_list')
+
+#bar chart for genre counts
+genre_counts = movies_exploded['genre_list'].value_counts()
+plt.figure(figsize = (12, 6))
+sns.barplot(x = genre_counts.index[:10], y = genre_counts.values[:10], palette = "coolwarm")
+plt.xticks(rotation = 45)
+plt.show()
+
+#countplot for languages
+plt.figure(figsize = (12, 6))
+sns.countplot(y = 'original_language', data = movies_df, order = movies_df['original_language'].value_counts(ascending = False).index[:15])
+plt.show()
+
+#separating actors and exploding
+movies_df['actors_list'] = movies_df['cast_names'].str.split(',')
+movies_exploded = movies_df.explode('actors_list')
+
+#bar chart for actor counts
+actors_count = movies_exploded['actors_list'].value_counts().sort_values(ascending = False)
+plt.figure(figsize = (12, 6))
+sns.barplot(x = actors_count.values[:15], y = actors_count.index[:15], orient = 'h')
+plt.yticks(rotation = 0)
+plt.show()
+
+#separating providers and exploding
+movies_df['providers_list'] = movies_df['watch_providers'].str.split(',')
+movies_exploded = movies_df.explode('providers_list')
+
+#grouping by providers_list and taking the mean of popularity
+provider_ratings = movies_exploded.groupby('providers_list')['popularity'].mean().sort_values(ascending = False)
+print(provider_ratings.head(15))
+
+#bar chart for providers based on popularity mean 
+plt.figure(figsize = (12,6))
+sns.barplot(x = provider_ratings.values[:15], y = provider_ratings.index[:15])
+plt.show()
+
+'''plt.figure(figsize = (12,6))
+plt.hist(movies_exploded['popularity'])
+plt.xlim(0,500)
+plt.show()'''
+
+
