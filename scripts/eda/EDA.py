0aba2902ff00981e8808e09ce98934ef45d1cdaf
lescer3 <schoolleslie3@gmail.com>
2025-03-05 16:41:02 -0800

    updated

--- scripts/eda/EDA.py
+++ scripts/eda/EDA.py
@@ -11,3 +11,9 @@ missing_values.sort_values(inplace = True)
 plt.figure(figsize = (14,10))
 missing_values.plot(kind = "barh", color = "green")
 plt.show()
+
+#plotting missing values 
+plt.figure(figsize = (18,8))
+sns.heatmap(movies_df.isnull(), cmap = sns.color_palette("viridis", as_cmap = True))
+plt.show()
+
